f6307549341c3b1c29afd2a746d5b7cec4202650
ODG <o.d.g@kakao.com>
2023-04-02 22:40:17 +0900

    feat : [010-Product-Modify] Product Modify, ProductItem Modify
    Product & ProductItem Modify

--- order-api/src/main/java/com/example/order/service/ProductService.java
+++ order-api/src/main/java/com/example/order/service/ProductService.java
@@ -2,11 +2,18 @@ package com.example.order.service;
 
 
 import com.example.order.domain.model.Product;
+import com.example.order.domain.model.ProductItem;
 import com.example.order.domain.product.AddProductForm;
+import com.example.order.domain.product.UpdateProductForm;
+import com.example.order.domain.product.UpdateProductItemForm;
 import com.example.order.domain.repository.ProductRepository;
+import com.example.order.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.example.order.exception.ErrorCode.NOT_FOUND_ITEM;
+import static com.example.order.exception.ErrorCode.NOT_FOUND_PRODUCT;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +24,26 @@ public class ProductService {
     @Transactional
     public Product addProduct(Long sellerId, AddProductForm addProductForm) {
         return productRepository.save(Product.of(sellerId, addProductForm));
+    }
+
+
+    @Transactional
+    public Product updateProduct(Long sellerId, UpdateProductForm form) {
+        Product product = productRepository.findBySellerIdAndId(sellerId, form.getId())
+                .orElseThrow(() -> new CustomException(NOT_FOUND_PRODUCT));
+
+        product.setName(form.getName());
+        product.setDescription(form.getDescription());
+
+        for (UpdateProductItemForm itemForm : form.getItems()) {
+            ProductItem productItem = product.getProductItems().stream()
+                    .filter(pi -> pi.getId().equals(itemForm.getId()))
+                    .findFirst().orElseThrow(() -> new CustomException(NOT_FOUND_ITEM));
+
+            productItem.setName(itemForm.getName());
+            productItem.setPrice(itemForm.getPrice());
+            productItem.setCount(itemForm.getCount());
+        }
+        return product;
     }
 }
